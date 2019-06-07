0cfec4014811c9adb6d0d10948d6ff578427be17
Rian Sanjaya <rian.sj@gmail.com>
2019-06-07 20:42:37 +0700

    view without auth

--- backend/controllers/stuff.js
+++ backend/controllers/stuff.js
@@ -48,43 +48,43 @@ exports.getOneThing = (req, res, next) => {
     });
 };
 
-exports.modifyThing = (req, res, next) => {
-    let thing = new Thing({ _id: req.params._id });
-    if (req.file) {
-      const url = req.protocol + '://' + req.get('host');
-      req.body.thing = JSON.parse(req.body.thing);
-      thing = {
-        _id: req.params.id,
-        title: req.body.thing.title,
-        description: req.body.thing.description,
-        imageUrl: url + '/images/' + req.file.filename,
-        price: req.body.thing.price,
-        userId: req.body.thing.userId
-      };
-    } else {
-      thing = {
-        _id: req.params.id,
-        title: req.body.title,
-        description: req.body.description,
-        imageUrl: req.body.imageUrl,
-        price: req.body.price,
-        userId: req.body.userId
-      };
-    }
-    Thing.updateOne({_id: req.params.id}, thing).then(
-      () => {
-        res.status(201).json({
-          message: 'Thing updated successfully!'
-        });
-      }
-    ).catch(
-      (error) => {
-        res.status(400).json({
-          error: error
-        });
-      }
-    );
-  };
+// exports.modifyThing = (req, res, next) => {
+//     let thing = new Thing({ _id: req.params._id });
+//     if (req.file) {
+//       const url = req.protocol + '://' + req.get('host');
+//       req.body.thing = JSON.parse(req.body.thing);
+//       thing = {
+//         _id: req.params.id,
+//         title: req.body.thing.title,
+//         description: req.body.thing.description,
+//         imageUrl: url + '/images/' + req.file.filename,
+//         price: req.body.thing.price,
+//         userId: req.body.thing.userId
+//       };
+//     } else {
+//       thing = {
+//         _id: req.params.id,
+//         title: req.body.title,
+//         description: req.body.description,
+//         imageUrl: req.body.imageUrl,
+//         price: req.body.price,
+//         userId: req.body.userId
+//       };
+//     }
+//     Thing.updateOne({_id: req.params.id}, thing).then(
+//       () => {
+//         res.status(201).json({
+//           message: 'Thing updated successfully!'
+//         });
+//       }
+//     ).catch(
+//       (error) => {
+//         res.status(400).json({
+//           error: error
+//         });
+//       }
+//     );
+//   };
 
 exports.modifyThing = (req, res, next) => {
     // we first create a new instance of our Thing model with the received _id 
--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -11,13 +11,21 @@ const stuffCtlr = require('../controllers/stuff');
 // Make sure you place multer after auth
 
 // GET route to return all items
-router.get('/', auth, stuffCtlr.getAllStuff);
+// router.get('/', auth, stuffCtlr.getAllStuff);
+router.get('/', stuffCtlr.getAllStuff);
+
+// GET  route to return a specific item without authentication
+router.get('/view/:id', stuffCtlr.getOneThing)
+
 // POST route to add a new item
 router.post('/', auth, multer, stuffCtlr.createThing);
+
 // GET route to return a specific item
 router.get('/:id', auth, stuffCtlr.getOneThing);
+
 // PUT route to edit a specific item
 router.put('/:id', auth, multer, stuffCtlr.modifyThing);
+
 // DELETE route to delete a specific item
 router.delete('/:id', auth, stuffCtlr.deleteThing);
 
